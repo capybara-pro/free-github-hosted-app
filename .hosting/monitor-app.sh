3692a37cc34e6439d1aefe00f55652586e713534
Alexander Salamatov <ch1seL@ya.ru>
2024-02-20 11:25:14 +0300

    refactor(monitor-app.sh): use docker compose instead of docker-compose

--- .hosting/monitor-app.sh
+++ .hosting/monitor-app.sh
@@ -3,7 +3,7 @@
 while true; do
   if [ -z "$(docker-compose ps -q app)" ] ; then
     echo "App is not running. Docker compose stop..."
-    docker-compose stop tunnel monitor
+    docker compose stop tunnel monitor
     exit 0
   fi
   sleep 10
